8d384732a172b6c27b04c09a50d0dabf33e79d25
hoioi <cjjs0919@naver.com>
2023-12-28 17:40:53 +0900

    mili

--- Intropage_slide/script.js
+++ Intropage_slide/script.js
@@ -11,17 +11,11 @@ fetch(url)
       let name = item.querySelector(".name");
       let en_name = item.querySelector(".en_name");
       let content_text = item.querySelector(".content_text");
-      let L_price = item.querySelector(".detail_L_price");
-      let M_price = item.querySelector(".detail_M_price");
-      let S_price = item.querySelector(".detail_S_price");
 
       name.innerHTML = infoData[i].name;
       img.style.backgroundImage = `url(${infoData[i].img})`;
       en_name.innerHTML = infoData[i].en_name;
       content_text.innerHTML = infoData[i].content_text;
-      L_price.innerHTML = infoData[i].L_price;
-      M_price.innerHTML = infoData[i].M_price;
-      S_price.innerHTML = infoData[i].S_price;
     });
   });
 
@@ -30,8 +24,6 @@ fetch(url)
 let nextButton = document.querySelector("#next");
 let prevButton = document.querySelector("#prev");
 let backButton = document.querySelector("#back");
-let seeMoreButton = document.querySelectorAll(".seeMore");
-console.log(seeMoreButton);
 let container = document.querySelector(".container");
 let itemBox = document.querySelector(".itemBox");
 let contentWrap = document.querySelector(".contentWrap");
